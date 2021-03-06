120b5ed4b24a20c15c0207e15dfbbc13e8fea50e
Joao Paulo Lethier <jplethier@gmail.com>
2021-03-06 19:36:28 -0300

    removed unecessary test

--- spec/rubocop/safe_migrations_spec.rb
+++ spec/rubocop/safe_migrations_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe RuboCop::SafeMigrations do
   it "has a version number" do
     expect(RuboCop::SafeMigrations::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
